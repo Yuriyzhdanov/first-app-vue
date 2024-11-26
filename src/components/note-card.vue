853e57c7d04894413bcebb183bd045dc40a42cd3
YouROCK <yourock88@meta.ua>
2024-11-26 18:56:34 +0200

    Fixed note-card

--- src/components/note-card.vue
+++ src/components/note-card.vue
@@ -2,14 +2,13 @@
   <div class="note-card">
     <div class="note-content">
       <textarea
-        ref="editingTextarea"
         v-if="note.isEditing"
         v-model="note.text"
-        v-on:blur="saveNote(idx)"
-        v-on:keyup.enter="saveNote(idx)"
-        type="text"
+        v-on:blur="note.isEditing = false"
+        ref="elTextarea"
+
       ></textarea>
-      <p v-else v-on:dblclick="EditNote(idx)">{{ note.text }}</p>
+      <p v-else v-on:dblclick="editNoteText">{{ note.text }}</p>
     </div>
     <div class="note-actions">
       <button v-on:click="handleClickButtonRemove(idx)">Удалить</button>
@@ -21,14 +20,6 @@
   export default {
     props: ['note', 'idx'],
     emits: ['removingNote'],
-    emits: ['savingNoteText'],
-
-    data() {
-      return {
-        isEditing: false,
-        editedText: this.note.text,
-      }
-    },
 
     methods: {
       handleClickButtonRemove(idx) {
@@ -36,18 +27,11 @@
       },
 
       editNoteText(idx) {
-        // this.notes[idx].isEditing = true
-        // this.$nextTick(() => {
-        //   this.$refs.editingTextarea[0].focus()
-        // })
-        this.editedText = this.note.text
-        this.$emit('savingNoteText', { idx: this.idx, text: this.editedText })
         this.note.isEditing = true
-      },
-
-      saveNote(idx) {
-        // this.$emit('savingNoteText', { idx: this.idx, text: this.editedText })
-        // this.notes[idx].isEditing = false
+        this.$nextTick(() => {
+          console.log(this.$refs.elTextarea)
+          this.$refs.elTextarea.focus()
+        })
       },
     },
   }
